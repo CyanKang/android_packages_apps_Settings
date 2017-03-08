AICP/packages_apps_AicpExtras

Slim Recents squash [2/2]
This is a squashed and AICPfied port of all slim recents related commits found on Slim's ng7.1 frameworks/opt/slim.

Squashed commits:

commit e3fa1e4c8835414cca61be0abdce426497821c3e
Author: Griffin Millender <griffinn.millender@gmail.com>
Date:   Wed Jan 18 19:00:58 2017 -0600

    disable and force on Show topmost task if screen pinning is enabled

    Change-Id: Ic72e5578cafba0f29f0c1bb3c8e89cc0e999a471

commit 70074643f839acddcd8e86cf666a873a0accf18c
Author: Griffin Millender <griffinn.millender@gmail.com>
Date:   Fri Jan 6 11:24:03 2017 -0600

    More settings cleanup

     * Use one fragment for most slim settings

     * Create SlimPreferenceManager to handle SlimPreferences

     * Allow preferences to depend on list preferences
       - use slim:listDependency="<list_setting_key>|<list_entry>"
           to disable a preference if a certain entry is selected
           separate multiple entries with a ":"

     * Allow preferences to be hidden based on integer or boolean
       - Use slim:hidePreference="<boolean>" to hide a preference
       - Use slim:hidePreferenceInt="<int>" to define a integer
          to hide a preference and use
          slim:hidePreferenceIntDependency="<int>" to define the
          integer that hidePreferenceInt equals to disable, default 0

     * Move more code to the preferences to get rid of dupe code

     * Remove SlimPreference if Intent is not null and does not exist

     * general cleanup

    Change-Id: I125105d99bab142efe656c1f6eec9a46f8fdac81

commit acd781ec171994374d479da13503627bb9c37d2f
Author: Griffin Millender <griffinn.millender@gmail.com>
Date:   Wed Jan 4 22:54:27 2017 -0600

    Rename package to "slim"

    Change-Id: I2f8e2c2a937b8d35079e218167436b47ccadb0b0

commit fbd0af9c1b854b22da97025725190fbe4a2e97ec
Author: Griffin Millender <griffinn.millender@gmail.com>
Date:   Sat Oct 22 21:41:20 2016 -0500

    Cleanup SlimSeekBarPreference

    Change-Id: I8e6bc087e9c968fcc67a6444e

src/com/lordclockan/aicpextras/SlimRecentPanel.java
@@ -1,0 +1,239 @@
+/*
+ * Copyright (C) 2015-2017 SlimRoms Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.lordclockan.aicpextras;
+
+import android.app.ActivityManager;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.ContentResolver;
+import android.content.DialogInterface;
+import android.database.ContentObserver;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.UserHandle;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceScreen;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MenuInflater;
+
+//import com.slim.settings.DialogCreatable;
+//import com.slim.settings.SettingsPreferenceFragment;
+import com.lordclockan.aicpextras.preferences.SystemSettingSwitchPreference;
+import com.lordclockan.aicpextras.widget.SeekBarPreferenceCham;
+import com.lordclockan.R;
+
+import net.margaritov.preference.colorpicker.ColorPickerPreference;
+
+public class SlimRecentPanel extends /*Slim*/PreferenceFragment implements /*DialogCreatable,*/
+        Preference.OnPreferenceChangeListener {
+
+    private static final String TAG = "RecentPanelSettings";
+
+    // Preferences
+    private static final String RECENTS_MAX_APPS = "recents_max_apps";
+    private static final String RECENT_PANEL_LEFTY_MODE =
+            "recent_panel_lefty_mode";
+    private static final String RECENT_PANEL_SCALE =
+            "recent_panel_scale";
+    private static final String RECENT_PANEL_EXPANDED_MODE =
+            "recent_panel_expanded_mode";
+    private static final String RECENT_PANEL_BG_COLOR =
+            "recent_panel_bg_color";
+    private static final String RECENT_CARD_BG_COLOR =
+            "recent_card_bg_color";
+    private static final String RECENT_CARD_TEXT_COLOR =
+            "recent_card_text_color";
+
+    private SeekBarPreferenceCham mMaxApps;
+    private SystemSettingSwitchPreference mRecentPanelLeftyMode;
+    private SeekBarPreferenceCham mRecentPanelScale;
+    private ListPreference mRecentPanelExpandedMode;
+    private ColorPickerPreference mRecentPanelBgColor;
+    private ColorPickerPreference mRecentCardBgColor;
+    private ColorPickerPreference mRecentCardTextColor;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.slim_recent_panel_settings);
+        initializeAllPreferences();
+
+        if (screenPinningEnabled()) {
+            SystemSettingSwitchPreference pref = (SystemSettingSwitchPreference) findPreference("recent_panel_show_topmost");
+            pref.setChecked(true);
+            pref.setEnabled(false);
+        }
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mRecentPanelLeftyMode) {
+            Settings.System.putInt(getContext().getContentResolver(),
+                    Settings.System.RECENT_PANEL_GRAVITY,
+                    ((Boolean) newValue) ? Gravity.LEFT : Gravity.RIGHT);
+            return true;
+        } else if (preference == mRecentPanelScale) {
+            Settings.System.putInt(getContext().getContentResolver(),
+                Settings.System.RECENT_PANEL_SCALE_FACTOR, Integer.valueOf(String.valueOf(newValue)));
+            return true;
+        } else if (preference == mRecentPanelExpandedMode) {
+            int value = Integer.parseInt((String) newValue);
+            Settings.System.putInt(getContext().getContentResolver(),
+                    Settings.System.RECENT_PANEL_EXPANDED_MODE, value);
+            return true;
+        } else if (preference == mRecentPanelBgColor) {
+            String hex = ColorPickerPreference.convertToARGB(
+                    Integer.valueOf(String.valueOf(newValue)));
+            if (hex.equals("#00ffffff")) {
+                preference.setSummary(R.string.default_string);
+            } else {
+                preference.setSummary(hex);
+            }
+            int intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(getContext().getContentResolver(),
+                    Settings.System.RECENT_PANEL_BG_COLOR,
+                    intHex);
+            return true;
+        } else if (preference == mRecentCardBgColor) {
+            String hex = ColorPickerPreference.convertToARGB(
+                    Integer.valueOf(String.valueOf(newValue)));
+            if (hex.equals("#00ffffff")) {
+                preference.setSummary(R.string.default_string);
+            } else {
+                preference.setSummary(hex);
+            }
+            int intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(getContext().getContentResolver(),
+                    Settings.System.RECENT_CARD_BG_COLOR,
+                    intHex);
+            return true;
+        } else if (preference == mRecentCardTextColor) {
+            String hex = ColorPickerPreference.convertToARGB(
+                    Integer.valueOf(String.valueOf(newValue)));
+            if (hex.equals("#00ffffff")) {
+                preference.setSummary(R.string.default_string);
+            } else {
+                preference.setSummary(hex);
+            }
+            int intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(getContext().getContentResolver(),
+                    Settings.System.RECENT_CARD_TEXT_COLOR,
+                    intHex);
+            return true;
+        } else if (preference == mMaxApps) {
+            Settings.System.putInt(getContext().getContentResolver(),
+                Settings.System.RECENTS_MAX_APPS, Integer.valueOf(String.valueOf(newValue)));
+            return true;
+        }
+        return false;
+    }
+
+    private boolean screenPinningEnabled() {
+        return Settings.System.getInt(getContext().getContentResolver(),
+                Settings.System.LOCK_TO_APP_ENABLED, 0) != 0;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        updateRecentPanelPreferences();
+    }
+
+    private void updateRecentPanelPreferences() {
+        final boolean recentLeftyMode = Settings.System.getInt(getContext().getContentResolver(),
+                Settings.System.RECENT_PANEL_GRAVITY, Gravity.RIGHT) == Gravity.LEFT;
+        mRecentPanelLeftyMode.setChecked(recentLeftyMode);
+
+        mMaxApps.setValue(Settings.System.getIntForUser(getContext().getContentResolver(),
+                Settings.System.RECENTS_MAX_APPS, ActivityManager.getMaxRecentTasksStatic(),
+                UserHandle.USER_CURRENT));
+
+        final int recentScale = Settings.System.getInt(getContext().getContentResolver(),
+                Settings.System.RECENT_PANEL_SCALE_FACTOR, 100);
+        mRecentPanelScale.setValue(recentScale);
+
+        final int recentExpandedMode = Settings.System.getInt(getContext().getContentResolver(),
+                Settings.System.RECENT_PANEL_EXPANDED_MODE, 0);
+        mRecentPanelExpandedMode.setValue(recentExpandedMode + "");
+    }
+
+    private void initializeAllPreferences() {
+        mRecentPanelLeftyMode =
+                (SystemSettingSwitchPreference) findPreference(RECENT_PANEL_LEFTY_MODE);
+        mRecentPanelLeftyMode.setOnPreferenceChangeListener(this);
+
+        mMaxApps = (SeekBarPreferenceCham) findPreference(RECENTS_MAX_APPS);
+        mMaxApps.setOnPreferenceChangeListener(this);
+
+        // Recent panel background color
+        mRecentPanelBgColor =
+                (ColorPickerPreference) findPreference(RECENT_PANEL_BG_COLOR);
+        mRecentPanelBgColor.setOnPreferenceChangeListener(this);
+        final int intColor = Settings.System.getInt(getContext().getContentResolver(),
+                Settings.System.RECENT_PANEL_BG_COLOR, 0x00ffffff);
+        String hexColor = String.format("#%08x", (0x00ffffff & intColor));
+        if (hexColor.equals("#00ffffff")) {
+            mRecentPanelBgColor.setSummary(R.string.default_string);
+        } else {
+            mRecentPanelBgColor.setSummary(hexColor);
+        }
+        mRecentPanelBgColor.setNewPreviewColor(intColor);
+
+        // Recent card background color
+        mRecentCardBgColor =
+                (ColorPickerPreference) findPreference(RECENT_CARD_BG_COLOR);
+        mRecentCardBgColor.setOnPreferenceChangeListener(this);
+        final int intColorCard = Settings.System.getInt(getContext().getContentResolver(),
+                Settings.System.RECENT_CARD_BG_COLOR, 0x00ffffff);
+        String hexColorCard = String.format("#%08x", (0x00ffffff & intColorCard));
+        if (hexColorCard.equals("#00ffffff")) {
+            mRecentCardBgColor.setSummary(R.string.default_string);
+        } else {
+            mRecentCardBgColor.setSummary(hexColorCard);
+        }
+        mRecentCardBgColor.setNewPreviewColor(intColorCard);
+
+        // Recent card text color
+        mRecentCardTextColor =
+                (ColorPickerPreference) findPreference(RECENT_CARD_TEXT_COLOR);
+        mRecentCardTextColor.setOnPreferenceChangeListener(this);
+        final int intColorText = Settings.System.getInt(getContext().getContentResolver(),
+                Settings.System.RECENT_CARD_TEXT_COLOR, 0x00ffffff);
+        String hexColorText = String.format("#%08x", (0x00ffffff & intColorText));
+        if (hexColorText.equals("#00ffffff")) {
+            mRecentCardTextColor.setSummary(R.string.default_string);
+        } else {
+            mRecentCardTextColor.setSummary(hexColorText);
+        }
+        mRecentCardTextColor.setNewPreviewColor(intColorText);
+
+        mRecentPanelScale =
+                (SeekBarPreferenceCham) findPreference(RECENT_PANEL_SCALE);
+        mRecentPanelScale.setOnPreferenceChangeListener(this);
+
+        mRecentPanelExpandedMode =
+                (ListPreference) findPreference(RECENT_PANEL_EXPANDED_MODE);
+        mRecentPanelExpandedMode.setOnPreferenceChangeListener(this);
+    }
+}

CyanKang/android_packages_apps_Settings

This is a squashed port of all slim recents related commits found on Slim's ng7.1 frameworks/opt/slim.

src/com/android/settings/cyankang/SlimRecentPanel.java
@@ -1,0 +1,239 @@
+/*
+ * Copyright (C) 2015-2017 SlimRoms Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.cyankang;
+
+import android.app.ActivityManager;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.ContentResolver;
+import android.content.DialogInterface;
+import android.database.ContentObserver;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.UserHandle;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceScreen;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MenuInflater;
+
+//import com.slim.settings.DialogCreatable;
+//import com.slim.settings.SettingsPreferenceFragment;
+import com.android.settings.cyankang.SystemSettingSwitchPreference;
+import com.android.settings.cyankang.CustomSeekBarPreference;
+import com.android.R;
+
+import net.margaritov.preference.colorpicker.ColorPickerPreference;
+
+public class SlimRecentPanel extends /*Slim*/PreferenceFragment implements /*DialogCreatable,*/
+        Preference.OnPreferenceChangeListener {
+
+    private static final String TAG = "RecentPanelSettings";
+
+    // Preferences
+    private static final String RECENTS_MAX_APPS = "recents_max_apps";
+    private static final String RECENT_PANEL_LEFTY_MODE =
+            "recent_panel_lefty_mode";
+    private static final String RECENT_PANEL_SCALE =
+            "recent_panel_scale";
+    private static final String RECENT_PANEL_EXPANDED_MODE =
+            "recent_panel_expanded_mode";
+    private static final String RECENT_PANEL_BG_COLOR =
+            "recent_panel_bg_color";
+    private static final String RECENT_CARD_BG_COLOR =
+            "recent_card_bg_color";
+    private static final String RECENT_CARD_TEXT_COLOR =
+            "recent_card_text_color";
+
+    private CustomSeekBarPreference mMaxApps;
+    private SystemSettingSwitchPreference mRecentPanelLeftyMode;
+    private CustomSeekBarPreference mRecentPanelScale;
+    private ListPreference mRecentPanelExpandedMode;
+    private ColorPickerPreference mRecentPanelBgColor;
+    private ColorPickerPreference mRecentCardBgColor;
+    private ColorPickerPreference mRecentCardTextColor;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.slim_recent_panel_settings);
+        initializeAllPreferences();
+
+        if (screenPinningEnabled()) {
+            SystemSettingSwitchPreference pref = (SystemSettingSwitchPreference) findPreference("recent_panel_show_topmost");
+            pref.setChecked(true);
+            pref.setEnabled(false);
+        }
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mRecentPanelLeftyMode) {
+            Settings.System.putInt(getContext().getContentResolver(),
+                    Settings.System.RECENT_PANEL_GRAVITY,
+                    ((Boolean) newValue) ? Gravity.LEFT : Gravity.RIGHT);
+            return true;
+        } else if (preference == mRecentPanelScale) {
+            Settings.System.putInt(getContext().getContentResolver(),
+                Settings.System.RECENT_PANEL_SCALE_FACTOR, Integer.valueOf(String.valueOf(newValue)));
+            return true;
+        } else if (preference == mRecentPanelExpandedMode) {
+            int value = Integer.parseInt((String) newValue);
+            Settings.System.putInt(getContext().getContentResolver(),
+                    Settings.System.RECENT_PANEL_EXPANDED_MODE, value);
+            return true;
+        } else if (preference == mRecentPanelBgColor) {
+            String hex = ColorPickerPreference.convertToARGB(
+                    Integer.valueOf(String.valueOf(newValue)));
+            if (hex.equals("#00ffffff")) {
+                preference.setSummary(R.string.default_string);
+            } else {
+                preference.setSummary(hex);
+            }
+            int intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(getContext().getContentResolver(),
+                    Settings.System.RECENT_PANEL_BG_COLOR,
+                    intHex);
+            return true;
+        } else if (preference == mRecentCardBgColor) {
+            String hex = ColorPickerPreference.convertToARGB(
+                    Integer.valueOf(String.valueOf(newValue)));
+            if (hex.equals("#00ffffff")) {
+                preference.setSummary(R.string.default_string);
+            } else {
+                preference.setSummary(hex);
+            }
+            int intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(getContext().getContentResolver(),
+                    Settings.System.RECENT_CARD_BG_COLOR,
+                    intHex);
+            return true;
+        } else if (preference == mRecentCardTextColor) {
+            String hex = ColorPickerPreference.convertToARGB(
+                    Integer.valueOf(String.valueOf(newValue)));
+            if (hex.equals("#00ffffff")) {
+                preference.setSummary(R.string.default_string);
+            } else {
+                preference.setSummary(hex);
+            }
+            int intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(getContext().getContentResolver(),
+                    Settings.System.RECENT_CARD_TEXT_COLOR,
+                    intHex);
+            return true;
+        } else if (preference == mMaxApps) {
+            Settings.System.putInt(getContext().getContentResolver(),
+                Settings.System.RECENTS_MAX_APPS, Integer.valueOf(String.valueOf(newValue)));
+            return true;
+        }
+        return false;
+    }
+
+    private boolean screenPinningEnabled() {
+        return Settings.System.getInt(getContext().getContentResolver(),
+                Settings.System.LOCK_TO_APP_ENABLED, 0) != 0;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        updateRecentPanelPreferences();
+    }
+
+    private void updateRecentPanelPreferences() {
+        final boolean recentLeftyMode = Settings.System.getInt(getContext().getContentResolver(),
+                Settings.System.RECENT_PANEL_GRAVITY, Gravity.RIGHT) == Gravity.LEFT;
+        mRecentPanelLeftyMode.setChecked(recentLeftyMode);
+
+        mMaxApps.setValue(Settings.System.getIntForUser(getContext().getContentResolver(),
+                Settings.System.RECENTS_MAX_APPS, ActivityManager.getMaxRecentTasksStatic(),
+                UserHandle.USER_CURRENT));
+
+        final int recentScale = Settings.System.getInt(getContext().getContentResolver(),
+                Settings.System.RECENT_PANEL_SCALE_FACTOR, 100);
+        mRecentPanelScale.setValue(recentScale);
+
+        final int recentExpandedMode = Settings.System.getInt(getContext().getContentResolver(),
+                Settings.System.RECENT_PANEL_EXPANDED_MODE, 0);
+        mRecentPanelExpandedMode.setValue(recentExpandedMode + "");
+    }
+
+    private void initializeAllPreferences() {
+        mRecentPanelLeftyMode =
+                (SystemSettingSwitchPreference) findPreference(RECENT_PANEL_LEFTY_MODE);
+        mRecentPanelLeftyMode.setOnPreferenceChangeListener(this);
+
+        mMaxApps = (CustomSeekBarPreference) findPreference(RECENTS_MAX_APPS);
+        mMaxApps.setOnPreferenceChangeListener(this);
+
+        // Recent panel background color
+        mRecentPanelBgColor =
+                (ColorPickerPreference) findPreference(RECENT_PANEL_BG_COLOR);
+        mRecentPanelBgColor.setOnPreferenceChangeListener(this);
+        final int intColor = Settings.System.getInt(getContext().getContentResolver(),
+                Settings.System.RECENT_PANEL_BG_COLOR, 0x00ffffff);
+        String hexColor = String.format("#%08x", (0x00ffffff & intColor));
+        if (hexColor.equals("#00ffffff")) {
+            mRecentPanelBgColor.setSummary(R.string.default_string);
+        } else {
+            mRecentPanelBgColor.setSummary(hexColor);
+        }
+        mRecentPanelBgColor.setNewPreviewColor(intColor);
+
+        // Recent card background color
+        mRecentCardBgColor =
+                (ColorPickerPreference) findPreference(RECENT_CARD_BG_COLOR);
+        mRecentCardBgColor.setOnPreferenceChangeListener(this);
+        final int intColorCard = Settings.System.getInt(getContext().getContentResolver(),
+                Settings.System.RECENT_CARD_BG_COLOR, 0x00ffffff);
+        String hexColorCard = String.format("#%08x", (0x00ffffff & intColorCard));
+        if (hexColorCard.equals("#00ffffff")) {
+            mRecentCardBgColor.setSummary(R.string.default_string);
+        } else {
+            mRecentCardBgColor.setSummary(hexColorCard);
+        }
+        mRecentCardBgColor.setNewPreviewColor(intColorCard);
+
+        // Recent card text color
+        mRecentCardTextColor =
+                (ColorPickerPreference) findPreference(RECENT_CARD_TEXT_COLOR);
+        mRecentCardTextColor.setOnPreferenceChangeListener(this);
+        final int intColorText = Settings.System.getInt(getContext().getContentResolver(),
+                Settings.System.RECENT_CARD_TEXT_COLOR, 0x00ffffff);
+        String hexColorText = String.format("#%08x", (0x00ffffff & intColorText));
+        if (hexColorText.equals("#00ffffff")) {
+            mRecentCardTextColor.setSummary(R.string.default_string);
+        } else {
+            mRecentCardTextColor.setSummary(hexColorText);
+        }
+        mRecentCardTextColor.setNewPreviewColor(intColorText);
+
+        mRecentPanelScale =
+                (CustomSeekBarPreference) findPreference(RECENT_PANEL_SCALE);
+        mRecentPanelScale.setOnPreferenceChangeListener(this);
+
+        mRecentPanelExpandedMode =
+                (ListPreference) findPreference(RECENT_PANEL_EXPANDED_MODE);
+        mRecentPanelExpandedMode.setOnPreferenceChangeListener(this);
+    }
+}